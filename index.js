d68a8528281491cbdda207498ced65db96699daa
Sushmita67 <biswakarmasushmita5@gmail.com>
2024-12-24 17:07:48 +0545

    added db.js
    Added db.js in config

--- index.js
+++ index.js
@@ -8,19 +8,6 @@ const app = express();
 // Import routes
 const adminRoutes = require("./routes/adminRoutes");
 const authRoutes = require("./routes/authRoutes");
-const productRoutes = require("./routes/productRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-// const inventoryRoutes = require("./routes/inventoryRoutes");
-// const reviewRoutes = require("./routes/reviewRoutes");
-// const cartRoutes = require("./routes/cartRoutes");
-const wishlistRoutes = require("./routes/wishlistRoutes");
-const userRoutes = require("./routes/userRoutes");
-// const paymentRoutes = require("./routes/paymentRoutes");
-// const returnRoutes = require("./routes/returnRoutes");
-// const couponRoutes = require("./routes/CuponRoutes");
-// const notificationRoutes = require("./routes/NotificationRouter");
-
-// :(
 
 // Connect to the database
 const connectDB = async () => {
@@ -39,22 +26,10 @@ app.use(cors()); // Enable CORS for all origins
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/uploads', express.static('uploads')); // Endpoint for Image Location
-
 // Use routes
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
-// app.use("/api/inventory", inventoryRoutes);
-// app.use("/api/reviews", reviewRoutes);
-// app.use("/api/cart", cartRoutes);
-app.use("/api/wishlist", wishlistRoutes);
-app.use("/api/users", userRoutes);
-// app.use("/api/payments", paymentRoutes);
-// app.use("/api/returns", returnRoutes);
-// app.use("/api/coupons", couponRoutes);
-// app.use("/api/notifications", notificationRoutes);
+
 
 const port = process.env.PORT || 5000;
 
